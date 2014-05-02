b7e1357d9d22b809c51650bdba94c9f590673675
Yun Wu <ywu@cs.utexas.edu>
2014-05-02 18:59:24 -0500

    more emoticons

--- FuzzyPreprocessPCFG.java
+++ FuzzyPreprocessPCFG.java
@@ -1,12 +1,11 @@
 import java.io.IOException;
 
-
-public class FuzzyPreprocessPCFG extends FuzzyPreprocess{
+public class FuzzyPreprocessPCFG extends FuzzyPreprocess {
 
 	public FuzzyPreprocessPCFG(String input, String output, String slang) {
 		super(input, output, slang);
 	}
-	
+
 	public static void main(String[] args) {
 
 		String inputpath = "/u/ywu/nlp/final/trainingandtestdata/tweetCorpus.txt";
@@ -24,7 +23,7 @@ public class FuzzyPreprocessPCFG extends FuzzyPreprocess{
 		}
 
 	}
-	
+
 	public String filter(String s) {
 		// case-insensitive
 		s = s.toLowerCase();
@@ -36,51 +35,84 @@ public class FuzzyPreprocessPCFG extends FuzzyPreprocess{
 		s = replaceUsername(s);
 
 		s = replaceHashtag(s);
-			
-		//s = splitPunc(s);
-		
-		//for pcfg
-		s = replaceEmoticons(s);
-		
+
+		// s = splitPunc(s);
+
+		// for pcfg
 		s = deletePuncs(s);
-		
+
 		s = addPeriod(s);
+
+		s = removeSingleton(s);
 		
-		s = removeSingular(s);
+		s = removeParen(s);
 		
+		s = replaceEmoticons(s);
+
 		return s;
 	}
-	
+
 	protected String deleteURL(String s) {
 		// replace all urls with URL
-		return s.replaceAll("(?i)(?:https?|ftps?)://[\\w/%.-]+", "");
+		return s.replaceAll("(?i)(?:https?|ftps?)://[\\w/%.-_\\?&=]+", "");
 	}
-	
-	protected String replaceEmoticons(String s){
-		s = s.replaceAll(":\\)", "I am happy.");
-		s = s.replaceAll(":\\(", "I am sad.");
-		s = s.replaceAll("<[3]+", "I love it.");
+
+	protected String replaceEmoticons(String s) {
+		String hat = "[<>]?"; // optional hat/brow
+		String eye = "[:;=8]"; // eyes
+		String nose = "[\\-o\\*\\']?"; // optional nose
+		String happymouth = "[\\)\\]dDpP]"; // happy mouth
+		String rhappymouth = "[\\(]";
+		String sadmouth = "[\\(\\[/|@]"; // sad mouth
+		String rsadmouth = "[\\)]";
+		String suprisemouth = "[oO]";
+
+		String happy = hat + eye + nose + happymouth + "|" + rhappymouth + nose
+				+ eye + hat;
+		String sad = hat + eye + nose + sadmouth + "|" + rsadmouth + nose
+				+ eye + hat;
+		String suprise = " " + hat + eye + nose + suprisemouth;
+		
+		String heart = " <[3]+";
+		String brokenheart = " </3";
+		
+		s = s.replaceAll(happy, " I am happy.");
+		s = s.replaceAll(sad, " I am sad.");
+		s = s.replaceAll(suprise, " I am surprised.");
+		s = s.replaceAll(heart, " I love it.");
+		s = s.replaceAll(brokenheart, " I do not love it.");
 		return s;
 	}
-	
-	protected String deletePuncs(String s){
+
+	protected String deletePuncs(String s) {
 		s = s.trim();
 		s = s.replaceAll("([:\"?!\\(\\);,.`~ ])\\1{1,}", "$1");
-		if(s.charAt(0)=='\"') return s.trim().substring(1, s.length()-1).trim();
-		else return s.trim();
+		if (s.charAt(0) == '\"')
+			return s.trim().substring(1, s.length() - 1).trim();
+		else
+			return s.trim();
 	}
-	
-	protected String addPeriod(String s){
+
+	protected String addPeriod(String s) {
 		String endPunc = "!?.])";
 		String midPunc = "#$%^&(_+=[\\|:\";,`\'~";
 		int len = s.length();
-		char c = s.charAt(len-1);
-		if(endPunc.indexOf(c) != -1) return s;
-		else if(midPunc.indexOf(c) != -1) return s.substring(0, len-1) + '.';
-		else return s+".";
+		char c = s.charAt(len - 1);
+		if (endPunc.indexOf(c) != -1)
+			return s;
+		else if (midPunc.indexOf(c) != -1)
+			return s.substring(0, len - 1) + '.';
+		else
+			return s + ".";
+	}
+
+	protected String removeSingleton(String s) {
+		s = s.replaceAll("([.!?\\)]) (\\w)* *[.!?\\)]", "$1");
+		s = s.replaceAll("\\b[a-zA-Z]\\.", ".");
+		return s;
 	}
 	
-	protected String removeSingular(String s){
-		return s.replaceAll("([.!?\\)]) (\\w)* *[.!?\\)]", "$1");
+	protected String removeParen(String s) {
+		return s.replaceAll("\\((.)*\\)", "");
 	}
 }
